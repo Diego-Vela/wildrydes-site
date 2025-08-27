5d7cfadf9f30ab7f811208db3908b3ee1d182852
Diego Vela <120430807+Diego-Vela@users.noreply.github.com>
2025-08-27 15:58:17 -0700

    Change Cognito user pool ID and client ID
    Updated Cognito user pool configuration values.

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_zrZIjr7U0', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7jm8o555h6bk56ahfk3grp1apn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_41ysc8mZF', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3gffrlmilpnnuc8feap5fcg15o', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
