5b5e0fb19c682098af06b562b0f7b811c3f450f1
Diego Vela <120430807+Diego-Vela@users.noreply.github.com>
2025-08-27 16:02:20 -0700

    Update userPoolClientId in config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_41ysc8mZF', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3gffrlmilpnnuc8feap5fcg15o', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '35i2p2id9gus9fnuvv95eh1f5c', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
